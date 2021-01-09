be087bafeedca4b5c5f208ed7f20c9210aefdb69
liweibo03 <liweibo03@kuaishou.com>
2021-01-09 23:27:29 +0800

    通过在方法上使用SS的注解来控制特定方法的访问权限

--- securitydemo1/src/main/java/com/zeek/security/securitydemo1/Securitydemo1Application.java
+++ securitydemo1/src/main/java/com/zeek/security/securitydemo1/Securitydemo1Application.java
@@ -3,9 +3,11 @@ package com.zeek.security.securitydemo1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @MapperScan("com.zeek.security.securitydemo1.mapper")
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled = true)
 public class Securitydemo1Application {
 
     public static void main(String[] args) {
--- securitydemo1/src/main/java/com/zeek/security/securitydemo1/controller/TestController.java
+++ securitydemo1/src/main/java/com/zeek/security/securitydemo1/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.zeek.security.securitydemo1.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,6 +24,20 @@ public class TestController {
         return "hello index";
     }
 
+    // 测试注解：
+    @RequestMapping("/update")
+    @ResponseBody
+    @Secured({"ROLE_sale", "ROLE_manager"})
+    public String update() {
+        return "hello update";
+    }
 
+    @RequestMapping("/preAuthorize")
+    @ResponseBody
+    @PreAuthorize("hasAnyAuthority('admins')")
+    public String preAuthorize() {
+        System.out.println("preAuthorize");
+        return "preAuthorize";
+    }
 
 }
