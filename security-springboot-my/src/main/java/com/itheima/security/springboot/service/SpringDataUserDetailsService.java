2f825e9a4c5edfdbdc680b5e9a9887b837667592
kiggggg <liweibooooo@126.com>
2021-01-23 16:21:19 +0800

    定义单独的UserDetailService用户信息查询类

---
+++ security-springboot-my/src/main/java/com/itheima/security/springboot/service/SpringDataUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.itheima.security.springboot.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2021-01-23
+ */
+@Service
+public class SpringDataUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        System.out.println("username:" + username);
+        UserDetails userDetails = User.withUsername("zhangsan").password("123").authorities("p1").build();
+        return userDetails;
+    }
+}
