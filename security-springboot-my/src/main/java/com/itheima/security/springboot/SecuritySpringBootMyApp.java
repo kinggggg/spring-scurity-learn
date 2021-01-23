b13237f54d38f7e7d0b6c4dc837c5679292b1552
kiggggg <liweibooooo@126.com>
2021-01-23 16:16:23 +0800

    SpringBoot集成Spring Security框架进行登录

--- security-springboot-my/src/main/java/com/itheima/security/springboot/SecuritySpringBootMyApp.java
+++ security-springboot-my/src/main/java/com/itheima/security/springboot/SecuritySpringBootMyApp.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  **/
 @SpringBootApplication
-public class SecuritySpringBootMyApp {
+public class  SecuritySpringBootMyApp {
     public static void main(String[] args) {
         SpringApplication.run(SecuritySpringBootMyApp.class,args);
     }
--- security-springboot-my/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
+++ security-springboot-my/src/main/java/com/itheima/security/springboot/config/WebSecurityConfig.java
@@ -6,32 +6,33 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Administrator
  * @version 1.0
  **/
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //定义用户信息服务（查询用户信息）
-/*
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("zhangsan").password("123").authorities("p1").build());
         manager.createUser(User.withUsername("lisi").password("456").authorities("p2").build());
         return manager;
     }
-*/
+
 
     //密码编码器
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
@@ -53,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()//允许表单登录
                 .loginProcessingUrl("/login")
                 .successForwardUrl("/login-success")//自定义登录成功的页面地址
-        .and()
+                .and()
                 .logout()
                 .logoutUrl("/logout");
 
